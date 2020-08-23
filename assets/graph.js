6eaade33fb3761abce30937f454e14b735e74335
Tyler Casperson <tylercasperson@gmail.com>
2020-08-22 23:16:11 -0600

    added events, mouse over, mouse out, on click

--- assets/graph.js
+++ assets/graph.js
@@ -39,6 +39,18 @@ const update = (data) => {
   legendGroup.call(legend);
   legendGroup.selectAll('text').attr('fill', 'white');
 
+  const tip = d3
+    .tip()
+    .attr('class', 'tip card')
+    .html((d) => {
+      let content = `<div class="name">${d.data.name}</div>`;
+      content += `<div class="cost">${d.data.cost}</div>`;
+      content += `<div class="delete">Click to delete</div>`;
+      return content;
+    });
+
+  graph.call(tip);
+
   // join enhanced (pie) data to path elements
   const paths = graph.selectAll('path').data(pie(data));
 
@@ -65,6 +77,19 @@ const update = (data) => {
     .transition()
     .duration(750)
     .attrTween('d', arcTweenEnter);
+
+  // add events
+  graph
+    .selectAll('path')
+    .on('mouseover', (d, i, n) => {
+      tip.show(d, n[i]);
+      handleMouseOver(d, i, n);
+    })
+    .on('mouseout', (d, i, n) => {
+      tip.hide();
+      handleMouseOut(d, i, n);
+    })
+    .on('click', handleClick);
 };
 
 //   data array and firestore
@@ -119,3 +144,22 @@ function arcTweenUpdate(d) {
     return arcPath(i(t));
   };
 }
+
+const handleMouseOver = (d, i, n) => {
+  d3.select(n[i])
+    .transition('changeSliceFill')
+    .duration(300)
+    .attr('fill', '#fff');
+};
+
+const handleMouseOut = (d, i, n) => {
+  d3.select(n[i])
+    .transition('changeSliceFill')
+    .duration(300)
+    .attr('fill', color(d.data.name));
+};
+
+const handleClick = (d) => {
+  const id = d.data.id;
+  db.collection('expenses').doc(id).delete();
+};
