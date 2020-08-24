3f409fab3af31f53361fd179b421b300cc6bb5fb
Tyler Casperson <tylercasperson@gmail.com>
2020-08-23 22:14:11 -0600

    added database

--- assets/index.js
+++ assets/index.js
@@ -9,10 +9,30 @@ var activity = 'cycling';
 btns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     activity = e.target.dataset.activity;
+
+    btns.forEach((btn) => btn.classList.remove('active'));
+    e.target.classList.add('active');
+
+    input.setAttributeNS('id', activity);
   });
 });
 
-btns.forEach((btn) => btn.classList.remove('active'));
-e.target.classList.add('active');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-input.setAttributeNS('id', activity);
+  const distance = parseInt(input.value);
+  if (distance) {
+    db.collection('activities')
+      .add({
+        distance,
+        activity,
+        date: new Date().toString(),
+      })
+      .then(() => {
+        error.textContent = '';
+        input.value = '';
+      });
+  } else {
+    error.textContent = 'Please enter a valid distance';
+  }
+});
