fb19822d4b52d2cda50b8afa99e6acc2cb0895e0
Tyler Casperson <tylercasperson@gmail.com>
2020-08-24 22:28:46 -0600

    setup listener to put data into the database

---
+++ assets/graph.js
@@ -0,0 +1,22 @@
+var data = [];
+
+db.collection('employees').onSnapshot((res) => {
+  res.docChanges().forEach((change) => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex((item) => item.id == doc.id);
+        data[index] = doc;
+        break;
+      case 'removed':
+        data = data.filter((item) => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+  });
+});
