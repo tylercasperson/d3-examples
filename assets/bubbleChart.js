89edf23a927e1e038962bbdaae89f98fe1aec126
Tyler Casperson <tylercasperson@gmail.com>
2020-08-24 21:26:24 -0600

    displays bubbles corresponding to the data on the screen

--- assets/bubbleChart.js
+++ assets/bubbleChart.js
@@ -40,3 +40,27 @@ const rootNode = stratify(data).sum((d) => d.amount);
 const pack = d3.pack().size([960, 700]).padding(5);
 
 const bubbleData = pack(rootNode).descendants();
+const color = d3.scaleOrdinal(['#d1c4e9', '#b39ddb', '#9575cd']);
+
+const nodes = graph
+  .selectAll('g')
+  .data(bubbleData)
+  .enter()
+  .append('g')
+  .attr('transform', (d) => `translate(${d.x}, ${d.y})`);
+
+nodes
+  .append('circle')
+  .attr('r', (d) => d.r)
+  .attr('stroke', 'white')
+  .attr('stroke-width', 2)
+  .attr('fill', (d) => color(d.depth));
+
+nodes
+  .filter((d) => !d.children)
+  .append('text')
+  .attr('text-anchor', 'middle')
+  .attr('dy', '0.3em')
+  .attr('fill', 'white')
+  .attr('font-size', (d) => d.value * 5)
+  .text((d) => d.data.name);
