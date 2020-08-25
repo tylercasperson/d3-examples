c9fa22eea17c9e011a206b723205bd51724e6e09
Tyler Casperson <tylercasperson@gmail.com>
2020-08-24 23:02:58 -0600

    data is display as a tree diagram on the screen

--- assets/graph.js
+++ assets/graph.js
@@ -11,14 +11,60 @@ const graph = svg.append('g').attr('transform', 'translate(50,50)');
 const stratify = d3
   .stratify()
   .id((d) => d.name)
-  .partentId((d) => d.parent);
+  .parentId((d) => d.parent);
 
 const tree = d3.tree().size([dims.width, dims.height]);
 
 const update = (data) => {
+  graph.selectAll('.node').remove();
+  graph.selectAll('.link').remove();
+
   const rootNode = stratify(data);
 
   const treeData = tree(rootNode);
+
+  const nodes = graph.selectAll('.node').data(treeData.descendants());
+
+  const links = graph.selectAll('.link').data(treeData.links());
+
+  links
+    .enter()
+    .append('path')
+    .attr('class', 'link')
+    .attr('fill', 'none')
+    .attr('stroke', '#aaa')
+    .attr('stroke-width', 2)
+    .attr(
+      'd',
+      d3
+        .linkVertical()
+        .x((d) => d.x)
+        .y((d) => d.y)
+    );
+
+  const enterNodes = nodes
+    .enter()
+    .append('g')
+    .attr('class', 'node')
+    .attr('transform', (d) => `translate(${d.x}, ${d.y})`);
+
+  enterNodes
+    .append('rect')
+    .attr('fill', '#aaa')
+    .attr('stroke', '#555')
+    .attr('stroke-width', 2)
+    .attr('height', 50)
+    .attr('width', (d) => d.data.name.length * 20)
+    .attr('transform', (d) => {
+      var x = d.data.name.length * 10;
+      return `translate(${-x},-25)`;
+    });
+
+  enterNodes
+    .append('text')
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'white')
+    .text((d) => d.data.name);
 };
 
 var data = [];
